b993d366b03d9c1e0719e4bf587d68e4b4cb4a0d
Tianfu <tianfu.zhang@uni-konstanz.de>
2024-09-27 11:40:48 +0200

    27/09/2024

--- topology_map_creator/src/TopologyMapNode.cpp
+++ topology_map_creator/src/TopologyMapNode.cpp
@@ -1,34 +1,34 @@
 //Define all the topic names that are required for topological mapping
 #include "rclcpp/rclcpp.hpp"
-#include "topology_map_creator/msg/Area2D.hpp"
+#include "topology_map_creator/msg/area2_d.hpp"
 
 class TopologyMap : public rclcpp::Node
 {
 public:
   TopologyMap() : Node("topology_map")
   {
-    patrol_area_subscription_ = this->create_subscription<my_custom_msgs::msg::Area2D>(
+    patrol_area_subscription_ = this->create_subscription<topology_map_creator::msg::Area2D>(
       "/patrolArea", 10, std::bind(&TopologyMap::patrolAreaCallback, this, std::placeholders::_1));
 
-    obstacle_area_subscription_ = this->create_subscription<my_custom_msgs::msg::Area2D>(
+    obstacle_area_subscription_ = this->create_subscription<topology_map_creator::msg::Area2D>(
       "/obstacleArea", 10, std::bind(&TopologyMap::obstacleAreaCallback, this, std::placeholders::_1));
   }
 
 private:
-  void patrolAreaCallback(const my_custom_msgs::msg::Area2D::SharedPtr msg)
+  void patrolAreaCallback(const topology_map_creator::msg::Area2D::SharedPtr msg)
   {
     RCLCPP_INFO(this->get_logger(), "Received patrol area with %zu points", msg->points.size());
     // 在这里处理 patrolArea 消息
   }
 
-  void obstacleAreaCallback(const my_custom_msgs::msg::Area2D::SharedPtr msg)
+  void obstacleAreaCallback(const topology_map_creator::msg::Area2D::SharedPtr msg)
   {
     RCLCPP_INFO(this->get_logger(), "Received obstacle area with %zu points", msg->points.size());
     // 在这里处理 obstacleArea 消息
   }
 
-  rclcpp::Subscription<my_custom_msgs::msg::Area2D>::SharedPtr patrol_area_subscription_;
-  rclcpp::Subscription<my_custom_msgs::msg::Area2D>::SharedPtr obstacle_area_subscription_;
+  rclcpp::Subscription<topology_map_creator::msg::Area2D>::SharedPtr patrol_area_subscription_;
+  rclcpp::Subscription<topology_map_creator::msg::Area2D>::SharedPtr obstacle_area_subscription_;
 };
 
 int main(int argc, char *argv[])
