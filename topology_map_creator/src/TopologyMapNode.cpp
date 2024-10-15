db69fbbc3daccfe64d7ceb0c6b2f068e903d83db
tianfu_swarm <tianfuzhang1028@gmail.com>
2024-10-15 11:50:41 +0200

    15/10/2024

--- topology_map_creator/src/TopologyMapNode.cpp
+++ topology_map_creator/src/TopologyMapNode.cpp
@@ -1,16 +1,17 @@
 #include "rclcpp/rclcpp.hpp"
 #include "topology_map_creator/msg/area2_d.hpp"
 #include "topology_map_creator/msg/matrix.hpp"
+#include "topology_map_creator/msg/obstacle.hpp"
 
 class TopologyMap : public rclcpp::Node
 {
 public:
   TopologyMap() : Node("topology_mapping")
   {
-    patrol_area_subscription_ = this->create_subscription<topology_map_creator::msg::Area2D>(
-        "/patrolArea", 10, std::bind(&TopologyMap::patrolAreaCallback, this, std::placeholders::_1));
+    map_area_subscription_ = this->create_subscription<topology_map_creator::msg::Area2D>(
+        "/mapArea", 10, std::bind(&TopologyMap::mapAreaCallback, this, std::placeholders::_1));
 
-    obstacle_area_subscription_ = this->create_subscription<topology_map_creator::msg::Area2D>(
+    obstacle_area_subscription_ = this->create_subscription<topology_map_creator::msg::Obstacle>(
         "/obstacleArea", 10, std::bind(&TopologyMap::obstacleAreaCallback, this, std::placeholders::_1));
 
     topology_map_matrix_ = this->create_publisher<topology_map_creator::msg::Matrix>("/topoMapMatrix", 10);
@@ -20,24 +21,24 @@ public:
   }
 
 private:
-
   static constexpr size_t max_saved_messages_ = 10; // max number of message
+  std::vector<topology_map_creator::msg::Area2D::SharedPtr> map_area_msgs_;
+  std::vector<topology_map_creator::msg::Obstacle::SharedPtr> obstacle_area_msgs_;
   std::vector<topology_map_creator::msg::Area2D::SharedPtr> patrol_area_msgs_;
-  std::vector<topology_map_creator::msg::Area2D::SharedPtr> obstacle_area_msgs_;
 
-  void patrolAreaCallback(const topology_map_creator::msg::Area2D::SharedPtr msg)
+  void mapAreaCallback(const topology_map_creator::msg::Area2D::SharedPtr msg)
   {
-    RCLCPP_INFO(this->get_logger(), "Received patrol area with %zu points", msg->points.size());
-    patrol_area_msgs_.push_back(msg);
-    if (patrol_area_msgs_.size() > max_saved_messages_)
+    RCLCPP_INFO(this->get_logger(), "Received map area with %zu points", msg->points.size());
+    map_area_msgs_.push_back(msg);
+    if (map_area_msgs_.size() > max_saved_messages_)
     {
-      patrol_area_msgs_.erase(patrol_area_msgs_.begin());
+      map_area_msgs_.erase(map_area_msgs_.begin());
     }
   }
 
-  void obstacleAreaCallback(const topology_map_creator::msg::Area2D::SharedPtr msg)
+  void obstacleAreaCallback(const topology_map_creator::msg::Obstacle::SharedPtr msg)
   {
-    RCLCPP_INFO(this->get_logger(), "Received obstacle area with %zu points", msg->points.size());
+    RCLCPP_INFO(this->get_logger(), "Received obstacle area with %zu obstacles", msg->obstacles.size());
     obstacle_area_msgs_.push_back(msg);
     if (obstacle_area_msgs_.size() > max_saved_messages_)
     {
@@ -47,16 +48,30 @@ private:
 
   void processAndPublish();
 
-  rclcpp::Subscription<topology_map_creator::msg::Area2D>::SharedPtr patrol_area_subscription_;
-  rclcpp::Subscription<topology_map_creator::msg::Area2D>::SharedPtr obstacle_area_subscription_;
+  void processPatrolArea();
+
+  rclcpp::Subscription<topology_map_creator::msg::Area2D>::SharedPtr map_area_subscription_;
+  rclcpp::Subscription<topology_map_creator::msg::Obstacle>::SharedPtr obstacle_area_subscription_;
   rclcpp::Publisher<topology_map_creator::msg::Matrix>::SharedPtr topology_map_matrix_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
+void TopologyMap::processPatrolArea()
+{
+  if (!map_area_msgs_.empty())
+  { // The area of the mapArea
+  
+  }
+
+  if (!obstacle_area_msgs_.empty())
+  { // the area of the obstacleArea
+  }
+}
+
 void TopologyMap::processAndPublish()
 {
   topology_map_creator::msg::Matrix matrix_msg;
-  if (!patrol_area_msgs_.empty() || !obstacle_area_msgs_.empty())
+  if (!map_area_msgs_.empty() || !obstacle_area_msgs_.empty())
   {
     topology_map_creator::msg::Matrix matrix_msg;
 
